4ffb5d566720d8db97eb6aa9f2a154f7d3cadbbb
fullstack-jay <rizqirezaardiansyah@gmail.com>
2022-06-30 18:10:37 +0700

    first commit

---
+++ word.js
@@ -0,0 +1,154 @@
+const wordList = [
+    {
+        word: "python",
+        hint: "programming language"
+    },
+    {
+        word: "guitar",
+        hint: "a musical instrument"
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention"
+    },
+    {
+        word: "venus",
+        hint: "planet of out solar system"
+    },
+    {
+        word: "gold",
+        hint: "a yellow precious metal"
+    },
+    {
+        word: "golang",
+        hint: "programming language"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    },
+    {
+        word: "jpeg",
+        hint: "a small image file format"
+    },
+    {
+        word: "svg",
+        hint: "a vector image format"
+    },
+    {
+        word: "server",
+        hint: "related to computer or system"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "apple",
+        hint: "mobile manufactur"
+    }, 
+    {
+        word: "email",
+        hint: "related to exchanging message"
+    },
+    {
+        word: "proxy",
+        hint: "related to server application"
+    },
+    {
+        word: "mars",
+        hint: "second smallest planet"
+    },
+    {
+        word: "rocket",
+        hint: "space vechile or missile"
+    },
+    {
+        word: "tesla",
+        hint: "unit of magnetic flux density"
+    },
+    {
+        word: "crypto",
+        hint: "related to cryptocurrency"
+    },
+    {
+        word: "flute",
+        hint: "a musical instrumen"
+    },
+    {
+        word: "mysql",
+        hint: "a relational database"
+    },
+    {
+        word: "venice",
+        hint: "famous city of waters"
+    },
+    {
+        word: "banana",
+        hint: "yellow long curved fruit"
+    },
+    {
+        word: "google",
+        hint: "famous search engine"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "png",
+        hint: "type of raster image file"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform"
+    },
+    {
+        word: "act",
+        hint: "the doing of a thing"
+    },
+    {
+        word: "chess",
+        hint: "game played by two players"
+    },
+    {
+        word: "hockey",
+        hint: "a famous outdoor game"
+    },
+    {
+        word: "island",
+        hint: "land surrounded by water"
+    },
+    {
+        word: "mental",
+        hint: "relating to the mind"
+    },
+    {
+        word: "joking",
+        hint: "humorous or flippant"
+    },
+    {
+        word: "gif",
+        hint: "a file format for image"
+    },
+    {
+        word: "avatar",
+        hint: "epic science fiction film"
+    },
+    {
+        word: "bugs",
+        hint: "related to programming"
+    },
+    {
+        word: "matrix",
+        hint: "science fiction movie",
+    },
+    {
+        word: "coding",
+        hint: "related to programming"
+    },
+]
